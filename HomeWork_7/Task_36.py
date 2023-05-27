2803a81ffdbfd1c03648cfad8419e9dbd6165ed2
RJMakarenko <propusk2@mail.ru>
2023-05-27 10:13:37 +0300

    Homework 7 v.2

--- HomeWork_7/Task_36.py
+++ HomeWork_7/Task_36.py
@@ -4,10 +4,3 @@ def print_operation_table(operation, num_rows=6, num_columns=6):
         for y in range(1, num_columns + 1):
             row.append(operation(x, y))
         print(*row, sep='\t')
-
-
-print_operation_table(lambda x, y: x * y)
-print()
-print_operation_table(lambda x, y: x * y, num_rows=4, num_columns=4)
-print()
-print_operation_table(lambda x, y: x + y, num_rows=3, num_columns=3)
